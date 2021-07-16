6b25dc3e12e9cdaa8a547cb5e1c7be9049ef1cd3
ixphinx <48696004+ixphinx@users.noreply.github.com>
2021-07-15 22:25:34 -0300

    update

--- src/models/database.js
+++ src/models/database.js
@@ -3,43 +3,23 @@ const {Schema} = mongoose;
 
 const Data = new Schema({
     
-    /*-----index-----*/
-    ipart:String,
-    iparc:String,
+    /*----- Divisionales-------*/
+    divisional: String,
     
     /*---- Cuentas de Usuarios ----*/
     user: String,
     password: String,
     username: String,
     userlastname: String,
-    userci: String,
     aproved: Boolean,
     usertype: String,
     
-    
-    /*----- Noticias---- */
-    noticias:String,
-
-    /*------ Comentarios -----*/
-    comhead: String,
-    combody: String,
-    comhead1: String,
-    combody1: String,
-    
-     /*------ Backgrounds -----*/
-    backgrounda: String,
-    backgroundb: String,
-    backgroundc: String,
-    backgroundfem: String,
-    backgroundf:String,
 
     /*---- Jugadores ----- */
     jnombre: String,
     jimg: String,
-    jescudo: String,
     japellido: String,
     jcedula: String,
-    jgenero: String,
     jgoles: Number,
     jtarjetasam: Number,
     jtarjetasdam: Number,
@@ -49,24 +29,12 @@ const Data = new Schema({
     jfechaamonestacion: String,
     jfechainicioamonestacion: String,
     jequipo: String,
-    jdivision: String,
 
     /*------ Equipos ----- */
     enombre: String,
     eescudo: String,
-    egenero: String,
     edivision: String,
-    edelegado: String,
-    edelcedula: String,
-    epuntos: Number,
-    epartidosj: Number,
-    epartidosg: Number,
-    epartidosempatados: Number,
-    epartidosperdidos: Number,
-    egolesmas: Number,
-    egolesmenos: Number,
-    esaldo: Number,
-    efairplay: Number,
+    
 
     /*------- Torneos ------*/
     tnombre: String,
@@ -79,8 +47,6 @@ const Data = new Schema({
 
 
     /*------- Partidos ------*/
-    ptorneo: String,
-    pfecha: String,
     pfechaid: String,
     pdate: String,
     ptime: String,
@@ -94,48 +60,25 @@ const Data = new Schema({
     pamarillasb: String,
     projasa: String,
     projasb: String,
-    pfpa: String,
-    pfpb: String,
+    pfpa: Number,
+    pfpb: Number,
     parbitro: String,
     pobservaciones: String,
     pcancha: String,
-    pid: String,
-    pjugador: String,
-    ppuntualidad: String,
-    pfairplay: String,
-    ppelota: String,
-    pindumentaria: String,
-    pformulario: String,
+    
+    // Estadisticas ///////////////////////////////////////////////////////
+    epartidoid: String,
+    eequipo: String,
+    ejugador: String,
+    egoles: String,
+    eamarillas: String,
+    erojas: String,
 
-    /*---- Seguridad------*/
-    paneladmin: String,
-    delegado:String,
-
-    /*----- Mensaje ----*/
-    msgemail: String,
-    msgbody: String,
-
-    /*---- Kaygua ----*/
-    msgname: String,
-    msgmail: String,
-    msgtel: String,
-    msgtext: String,
+    
     
     /*------ imagenes ----- */
     imgdiv: String,
     imgurl: String,
-    
-    /*------ Estadisticas ------*/
-    estIdPartido: String,
-    estIdEquipo: String,
-    estIdJugador: String,
-    estNombre: String,
-    estApellido: String,
-    estAmarilla: Number,
-    estDobAmarilla: Number,
-    estRoja: Number,
-    estGol: Number,
-    estAmonestacion: Number
 
 
 });
